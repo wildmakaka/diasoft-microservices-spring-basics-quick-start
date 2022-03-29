6f793c8abac682cf54ce2aee2e7696684cd1606c
webmak1 <marley.ruby.on.rails@gmail.com>
2022-03-29 05:48:03 +0300

    App03

--- apps/app03/src/main/java/org/javadev/restservice/AppController.java
+++ apps/app03/src/main/java/org/javadev/restservice/AppController.java
@@ -1,16 +1,89 @@
 package org.javadev.restservice; 
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
+@RequestMapping("apples")
 public class AppController {
+	
+	private int counter = 4;
 
 	@GetMapping("/hi")
 	public String hi(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return name;
 	}
-}
+	
+	// -------------
+	
+	private List<Map<String, String>> apples = new ArrayList<Map<String, String>>() {{
+			add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First apple"); }});
+			add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second apple"); }});
+			add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third apple"); }});
+	}};
+
+	@GetMapping
+	  public List<Map<String, String>> list() {
+	    return apples;
+	  }
+	
+	
+	// -----------
+	
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return apples.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+    
+    // ------------
+    
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        apples.add(message);
+        return message;
+    }
+    
+    // ------------
+    
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+
+        return messageFromDb;
+    }
+    
+    // ------------
+    
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+
+        apples.remove(message);
+    }
+	
+	
+} // The End of Class;
--- docs/App03.md
+++ docs/App03.md
@@ -16,3 +16,160 @@ $ cd apps/app03
 $ ./mvnw spring-boot:run
 ```
 
+
+<br/>
+
+```
+// GET ALL
+$ curl \
+    --header "Content-Type: application/json" \
+    --request GET \
+    --url http://localhost:8080/apples \
+    | jq
+```
+
+<br/>
+
+**returns:**
+
+```
+[
+  {
+    "id": "1",
+    "text": "First apple"
+  },
+  {
+    "id": "2",
+    "text": "Second apple"
+  },
+  {
+    "id": "3",
+    "text": "Third apple"
+  }
+]
+```
+
+
+<br/>
+
+```
+// GET BY ID
+$ curl \
+    --header "Content-Type: application/json" \
+    --request GET \
+    --url http://localhost:8080/apples/2 \
+    | jq
+```
+
+
+<br/>
+
+**returns:**
+
+```
+{
+  "id": "2",
+  "text": "Second apple"
+}
+```
+
+<br/>
+
+```
+// ADD Single
+$ curl \
+    --data '{
+      "text":"Fourth apple"
+      }' \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --url http://localhost:8080/apples \
+    | jq
+```
+
+
+<br/>
+
+**returns:**
+
+```
+{
+  "text": "Fourth apple",
+  "id": "4"
+}
+```
+
+
+<br/>
+
+```
+// UPDATE Single
+$ curl \
+    --data '{
+      "text":"Fifth apple"
+      }' \
+    --header "Content-Type: application/json" \
+    --request PUT \
+    --url http://localhost:8080/apples/2 \
+    | jq
+```
+
+
+<br/>
+
+**returns:**
+
+```
+{
+  "id": "2",
+  "text": "Fifth apple"
+}
+```
+
+
+<br/>
+
+```
+// DELETE Single
+$ curl \
+    --header "Content-Type: application/json" \
+    --request DELETE \
+    --url http://localhost:8080/apples/1 \
+    | jq
+```
+
+
+<br/>
+
+**returns:**
+
+```
+Nothing
+```
+
+<br/>
+
+```
+// GET ALL
+```
+
+<br/>
+
+**returns:**
+
+```
+[
+  {
+    "id": "2",
+    "text": "Fifth apple"
+  },
+  {
+    "id": "3",
+    "text": "Third apple"
+  },
+  {
+    "text": "Fourth apple",
+    "id": "4"
+  }
+]
+```
